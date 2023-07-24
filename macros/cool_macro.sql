98fdf1247e89d3952157721d1609b13ccfb7589f
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-24 13:53:39 -0600

    Update cool_macro.sql

--- macros/cool_macro.sql
+++ macros/cool_macro.sql
@@ -1,1 +1,15 @@
+{% macro my_schema_relations() %}
 
+{% set my_schema_relations = dbt_utils.get_relations_by_pattern(
+    schema_pattern='dbt_%',
+    table_pattern='%'
+) %}
+
+{% set sql_to_execute = [] %}
+
+{{ log('We found these tables that match your patterns:', info=True) }}
+
+{% for relation in my_schema_relations %}
+    {% do log(relation, info=True) %}
+{% endfor %}
+{% endmacro %}
