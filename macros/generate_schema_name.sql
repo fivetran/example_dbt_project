1dee41cdd78af543ad2245226eaed7fddb70ece5
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-24 13:33:07 -0600

    Create generate_schema_name.sql

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,16 @@
+For more info on this macro please see the documentation https://docs.getdbt.com/docs/build/custom-schemas
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
