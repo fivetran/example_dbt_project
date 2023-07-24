6d2d50259a34e9f4be918819747bd717a0168884
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-24 14:22:46 -0600

    Update intermediate_model.sql

--- models/1_intermediate/intermediate_model.sql
+++ models/1_intermediate/intermediate_model.sql
@@ -1,1 +1,15 @@
+with example_one as (
 
+  select * from {{ ref('example_one') }}
+
+  )
+,
+
+example_two as (
+
+  select * from {{ ref('example_two') }}
+  
+  )
+
+select * from example one
+  left join example_two on example_two.id = example_one.team_id
