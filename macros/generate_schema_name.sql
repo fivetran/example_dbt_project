465b19dda58cb17fcd2fbc56fcd6250acc50ac63
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-24 13:57:43 -0600

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,5 +1,3 @@
-For more info on this macro please see the documentation https://docs.getdbt.com/docs/build/custom-schemas
-
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
@@ -12,5 +10,5 @@ For more info on this macro please see the documentation https://docs.getdbt.com
         {{ default_schema }}_{{ custom_schema_name | trim }}
 
     {%- endif -%}
-
+{{ log('For more info on this macro please see the documentation https://docs.getdbt.com/docs/build/custom-schemas', info=True) }}
 {%- endmacro %}
