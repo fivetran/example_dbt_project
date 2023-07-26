9aecb42a2f2e9115a09c68666f7c6866bd02ede9
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-26 15:04:43 -0600

    Update my_custom_test.sql

--- tests/my_custom_test.sql
+++ tests/my_custom_test.sql
@@ -1,5 +1,5 @@
 with make_sure as (
-  select count(*) as num_records from {{ ref('example_two' )}}
+  select count(*) as num_records from {{ ref('staging_example_two' )}}
   )
 
 select * from make_sure where num_records > 1
