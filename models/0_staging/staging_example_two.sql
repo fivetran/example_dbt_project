f5479a96c9d47cfe372b96cbded0ec77c3ad8bd7
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-26 14:59:11 -0600

    Update staging_example_two.sql

--- models/0_staging/staging_example_two.sql
+++ models/0_staging/staging_example_two.sql
@@ -13,9 +13,11 @@
 -- ) }}
 
 with source_data as (
-    select * from {{ source('sql_server_dbo', 'customers') }}
-    where id = 12345
+    select max(_fivetran_synced) as most_recent_sync from {{ source('connector_name_schema_name', 'other_table_name') }}
+    where _fivetran_deleted = 'False'
 )
 
-select *
+select
+'source_table_two' as table_name
+, most_recent_sync
 from source_data
