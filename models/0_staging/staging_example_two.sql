e85ca3d0bfdef8f0e66f2fa19b0fc6a145aa2274
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-28 09:38:51 -0600

    Update staging_example_two.sql

--- models/0_staging/staging_example_two.sql
+++ models/0_staging/staging_example_two.sql
@@ -1,17 +1,3 @@
--- {{ config(
---     enabled=true | false,
---     tags="<string>" | ["<string>"],
---     pre_hook="<sql-statement>" | ["<sql-statement>"],
---     post_hook="<sql-statement>" | ["<sql-statement>"],
---     database="<string>",
---     schema="<string>",
---     alias="<string>",
---     persist_docs={<dict>},
---     meta={<dict>},
---     grants={<dict>},
---     contract={<dictionary>}
--- ) }}
-
 with source_data as (
     select max(_fivetran_synced) as most_recent_sync from {{ source('connector_name_schema_name', 'other_table_name') }}
     where _fivetran_deleted = 'False'
