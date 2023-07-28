0cd96d4d62cae9f8e0e5ab64328b9c7affaa85b7
Jack Lowery <97244562+fivetran-jacklowery@users.noreply.github.com>
2023-07-28 09:38:34 -0600

    Update staging_example_one.sql

--- models/0_staging/staging_example_one.sql
+++ models/0_staging/staging_example_one.sql
@@ -1,16 +1,20 @@
--- {{ config(
---     enabled=true | false,
---     tags="<string>" | ["<string>"],
---     pre_hook="<sql-statement>" | ["<sql-statement>"],
---     post_hook="<sql-statement>" | ["<sql-statement>"],
---     database="<string>",
---     schema="<string>",
---     alias="<string>",
---     persist_docs={<dict>},
---     meta={<dict>},
---     grants={<dict>},
---     contract={<dictionary>}
--- ) }}
+/*
+There are many additional configurations for models, here are a few examples:
+{{ config(
+    enabled=true | false,
+    tags="<string>" | ["<string>"],
+    pre_hook="<sql-statement>" | ["<sql-statement>"],
+    post_hook="<sql-statement>" | ["<sql-statement>"],
+    database="<string>",
+    schema="<string>",
+    alias="<string>",
+    persist_docs={<dict>},
+    meta={<dict>},
+    grants={<dict>},
+    contract={<dictionary>}
+) }}
+
+*/
 
 with source_data as (
     select max(_fivetran_synced) as most_recent_sync from {{ source('connector_name_schema_name', 'table_name') }}
